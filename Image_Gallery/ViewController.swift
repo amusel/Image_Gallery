332f9352648a867dc1bafdc0da8a16d6973d439e
amusel <a.musel98@gmail.com>
2019-09-06 13:23:55 +0300

    Initial Commit

---
+++ Image_Gallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Image_Gallery
+//
+//  Created by Artem Musel on 9/6/19.
+//  Copyright © 2019 Artem Musel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
